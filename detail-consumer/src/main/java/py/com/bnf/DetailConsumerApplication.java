52099462fa17240f0c8ff395db60cdb9b112473b
Santiago Vargas <sai19.sv@gmail.com>
2025-08-28 23:29:46 -0300

    Se agrega detail-consumer

---
+++ detail-consumer/src/main/java/py/com/bnf/DetailConsumerApplication.java
@@ -0,0 +1,13 @@
+package py.com.bnf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailConsumerApplication.class, args);
+	}
+
+}
